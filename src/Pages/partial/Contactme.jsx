9d3be6212b0a3e791f9b8d875e476a297ec035ca
Ka3chi <aldrinalmoradie69@gmail.com>
2024-08-12 21:27:32 +0800

    added new

---
+++ src/Pages/partial/Contactme.jsx
@@ -0,0 +1,104 @@
+import { Button, Input, Textarea, Typography } from "@material-tailwind/react";
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { IoIosMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+
+export function Contactme() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <section id="contactus" data-aos="fade-up" data-aos-anchor-placement="center-bottom" className="px-8 py-8 lg:py-16">
+      <div className="container mx-auto text-center">
+        <Typography
+          variant="h1"
+          color="blue-gray"
+          className="mb-4 !text-3xl lg:!text-5xl"
+        >
+          Contact Me
+        </Typography>
+        <Typography className="mb-10 font-normal !text-lg lg:mb-20 mx-auto max-w-3xl !text-gray-500">
+          Whether it&apos;s a question about our services, a request for
+          technical assistance, or suggestions for improvement, our team is
+          eager to hear from you.
+        </Typography>
+        <div className="grid grid-cols-1 gap-x-12 gap-y-6 lg:grid-cols-2 items-start">
+          <div className="bg-text w-full h-[425px] lg:max-h-[510px] rounded-xl p-[50px] text-white bg-black">
+            <div className="flex flex-col">
+              <div className="mb-[30px]">
+                <h1 className="font-bold text-[30px] text-left">Contact Information</h1>
+                <p className="text-[20px] font-thin text-left">Fill up the form and our Team will get back to you within 24 hours.</p>
+              </div>
+              <div className="flex flex-row items-center m-3">
+                <FaPhone className="h-6 w-6"/><p className="text-[18px] font-thin ml-5">+6309475607096</p>
+              </div>
+              <div className="flex flex-row items-center m-3">
+                <IoIosMail className="h-6 w-6"/><p className="text-[18px] font-thin ml-5">aldrinjohnalmoradie18@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <form
+            action="#"
+            className="flex flex-col gap-4 lg:max-w-sm space-y-6 pt-5"
+          >
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <Input
+                  label="First Name" 
+                  color="gray"
+                  size="lg"
+                  name="first-name"
+                  className="focus:border-t-gray-900"
+                  containerProps={{
+                    className: "min-w-full",
+                  }}
+                />
+              </div>
+              <div>
+                <Input
+                  label="Last Name" 
+                  color="gray"
+                  size="lg"
+                  name="last-name"
+                  className="focus:border-t-gray-900"
+                  containerProps={{
+                    className: "!min-w-full",
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <Input
+                label="Email" 
+                color="gray"
+                size="lg"
+                name="email"
+                className="focus:border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+              />
+            </div>
+            <div>
+              <Textarea
+                label="Message" 
+                rows={6}
+                color="gray"
+                name="message"
+                className="focus:border-t-gray-900 border-t-2"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+              />
+            </div>
+            <Button className="w-full" color="gray">
+              Send message
+            </Button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Pages/partial/Home.jsx
+++ src/Pages/partial/Home.jsx
@@ -1,28 +1,35 @@
 import React from "react"; 
 import picture from "../../Assets/Aldrin-picture.png";
 import { Button } from "@material-tailwind/react";
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFacebook  } from "react-icons/fa";
 import Typewriter from 'typewriter-effect';
+import { Contactme } from "./Contactme";
 
 export const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center p-0 m-0">
       {/* //Hero section */}
       <section className="flex h-[800px] w-full justify-center items-center">
-        <div className="flex flex-row items-center justify-center h-[500px]">
-          <div className="flex flex-col space-y-2 justify-center items-center">
-            <Button className="btn-button"><FaLinkedin /></Button>
-            <Button className="btn-button"><FaLinkedin /></Button>
-            <Button className="btn-button"><FaLinkedin /></Button>
+        <div className="flex flex-row items-center justify-start h-[500px]">
+          <div className="flex flex-col space-y-5 justify-center items-center">
+            <a className="btn-button flex w-[50px] h-[50px] justify-center items-center rounded-3xl" href="https://www.linkedin.com/in/aldrin-john-almoradie-41a50a2a8/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <FaLinkedin className="h-[30px] w-[30px]" />
+            </a>
+            <a className="btn-button flex w-[50px] h-[50px] justify-center items-center rounded-3xl" href="https://www.facebook.com/AldrinJohnAlmoradie/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <FaFacebook className="h-[30px] w-[30px]"/>
+            </a>
+            <a className="btn-button flex w-[50px] h-[50px] justify-center items-center rounded-3xl" href="https://github.com/Ka3chi" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <FaGithub className="h-[30px] w-[30px]"/>
+            </a>
           </div>
           <div>
-            <img src={picture} alt="profile" className="h-[400px] w-[400px]" />
+            <img src={picture} alt="profile" className="h-[400px] w-[390px]" />
           </div>
-          <div className="flex flex-col w-[400px] space-y-3 justify-center items-center">
-            <p className="text-[30px] p-0 m-0 w-full">
+          <div className="flex flex-col w-[500px] space-y-3">
+            <p className="text-[30px] p-0 m-0 w-full justify-start items-start ">
               Hi, I'm <span className="font-semibold">Aldrin John Almoradie</span>
             </p>
-            <div className="text-[30px] text-wrap flex flex-row justify-start items-start">
+            <div className="text-[30px] text-wrap flex flex-row justify-start items-start ">
               <p>I’m a</p>
               <span className="ml-2 text-[30px] font-semibold">
                 <Typewriter
@@ -35,9 +42,11 @@ export const Home = () => {
               </span>
             </div>
             <p className="text-wrap text-[20px] ">
-              With my experience as a Web App Developer Intern, I am pursuing a career as a Frontend Developer with the goal of becoming a Full-Stack Developer.
+            With my experience as a Web App Developer Intern, I aim to build a career in Information Technology, where I can leverage my expertise, apply my current skills, and grow further through continuous learning and hands-on experience.
             </p>
-            <Button className="btn-button hover:bg-white">Download CV</Button>
+            <div className="flex justify-center items-center">
+            <Button className="btn-button hover:bg-white font-extrabold text-[15px] ">Download CV</Button>
+            </div>
           </div>
         </div>
       </section>
@@ -46,11 +55,11 @@ export const Home = () => {
         <div className="flex justify-center items-center h-[800px] w-full bg-background1">
           <div className="flex flex-col justify-center items-center bg-white h-[500px] w-[900px] rounded-lg">
             <div className="flex justify-center items-center">
-              <div className="bg-blue-500 w-[40px] h-[40px] absolute top-[1200px] right-[650px]"></div>
+              <div className="bg-blue-500 w-[45px] h-[45px] absolute top-[1175px] right-[640px]"></div>
               <p className="font-semibold text-[50px] flex justify-center flex-wrap z-0 ">About Me</p>
             </div>
             <div className="flex justify-center items-center w-[800px]">
-              <p className="text-wrap font-thin text-[20px]">I am a web developer who enjoys exploring and discovering new things. I am passionate about learning and continuously improving my skills. I am always eager to learn new technologies and expand my knowledge. I aim to pursue a career in Information Technology, where I can leverage my expertise, apply my current skills, and develop further through ongoing training and practical experience.</p>
+              <p className="text-wrap font-thin text-[20px]">I am a web developer who enjoys exploring and discovering new things. I am passionate about learning and continuously improving my skills. I am always eager to learn new technologies and expand my knowledge.</p>
             </div>
           </div>
         </div>
@@ -64,8 +73,10 @@ export const Home = () => {
         <div className="flex justify-center items-center h-[800px] w-full">Projects</div>
       </section>
       {/* //Contact Me */}
-      <section className="flex justify-center items-center p-0 m-0 w-full bg-red-300">
-        <div className="flex justify-center items-center h-[800px] w-full">Contact Me</div>
+      <section className="flex justify-center items-center p-0 m-0 w-full">
+        <div className="flex justify-center items-center h-[800px] w-full">
+          <Contactme />
+        </div>
       </section>
     </div>
   );
